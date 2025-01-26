fb547df79837904f07dc98c3e9106494cbdc401c
Underwood, Scott F <scott.underwood@pnnl.gov>
2025-01-25 19:03:37 -0500

    initial commit

---
+++ get_data.py
@@ -0,0 +1,60 @@
+from datetime import datetime, timedelta
+# from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
+import requests
+
+
+def get_current_games(sport, teams, utc_offset):
+    urls = {'nfl': 'https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard',
+            'nba': 'https://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard',
+            'ncaafb': 'https://site.api.espn.com/apis/site/v2/sports/football/college-football/scoreboard',
+            'ncaabb': 'https://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/scoreboard',
+            'mlb': 'https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard'}
+
+    response = requests.get(urls[sport])
+    games = []
+    todays_date = datetime.now()
+    for event in response.json()['events']:
+        event_date = datetime.strptime(event['date'], '%Y-%m-%dT%H:%MZ') + timedelta(hours=utc_offset, days=-1)
+        for team in teams:
+            if team in event['name'] and event_date.date() == todays_date.date():
+                competition = event['competitions'][0]
+                # store general game info
+                game = {'time': event_date.time(), 
+                        'status': event['status']['type']['name'],
+                        'clock': event['status']['displayClock'],
+                        'period': event['status']['period']}
+                if sport == 'nfl' or sport == 'ncaafb':
+                    game['down'] = competition.get('situation', {}).get('shortDownDistanceText'),
+                    game['spot'] = competition.get('situation', {}).get('possessionText'),
+                    game['possession'] = competition.get('situation', {}).get('possession')
+                # store the individual team info
+                for competitor in competition['competitors']:
+                    if competitor['homeAway'] == 'home':
+                        game['home_location'] = competitor['team']['location']
+                        game['home_team'] = competitor['team']['name']
+                        game['home_logo'] = competitor['team']['logo']
+                        game['home_score'] = competitor['score']
+                    else:
+                        game['away_location'] = competitor['team']['location']
+                        game['away_team'] = competitor['team']['name']
+                        game['away_logo'] = competitor['team']['logo']
+                        game['away_score'] = competitor['score']
+
+                games.append(game)
+
+    return games
+
+
+
+if __name__ == '__main__':
+    UTC_OFFSET = -5
+    NFL_TEAMS = ['Green Bay Packers', 'Washington Commanders']
+    NBA_TEAMS = ['Milwaukee Bucks', 'Cleveland Cavaliers']
+    NCAAFB_TEAMS = ['Wisconsin Badgers']
+    NCAABB_TEAMS = ['Wisconsin Badgers', 'Marquette Golden Eagles']
+    MLB_TEAMS = ['Milwaukee Brewers', 'Chicago Cubs']
+
+
+    nfl_games = get_current_games('nfl', NFL_TEAMS, UTC_OFFSET)
+    print(nfl_games)
+
---
+++ run_display.py
@@ -0,0 +1,61 @@
+from get_data import get_current_games
+from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
+
+def run_display(api_key, station_code_receiver, direction_receiver, font_file):
+    # station code and direction will be sent on init
+    nfl_games = get_current_games('nfl', ['Green Bay Packers', 'Washington Commanders'], -5)
+    canvas = init_matrix()
+
+    # prev_lines = []
+    # prev_cars = []
+    # prev_dests = []
+    # prev_times = []
+
+    draw_display(canvas, font_file, [], [], [], [])
+
+    # while True:
+    #     force_update = False
+    #     if station_code_receiver.poll():
+    #         station_code = station_code_receiver.recv()
+    #     if direction_receiver.poll():
+    #         direction = direction_receiver.recv()
+    #     if incidents_check_count == 12: # check for incidents every minute
+    #         force_update = True
+    #         station = get_station_by_code(station_code)
+    #         if station == None:
+    #             logging.error("Could not find station for code: {}", station_code)
+    #         line_codes = get_line_codes_from_station(station)
+    #         incidents = get_incidents(line_codes, api_key)
+    #         for incident in incidents:
+    #             logging.info("Calling draw_incident for: {}".format(incident))
+    #             draw_incident(canvas, font_file, incident)
+    #         incidents_check_count = 0
+
+    #     prev_lines, prev_cars, prev_dests, prev_times = show_train_times(api_key, font_file, canvas, station_code, direction, prev_lines, prev_cars, prev_dests, prev_times, force_update)
+        
+    #     time.sleep(5)
+    #     incidents_check_count += 1
+
+
+def init_matrix():
+    options = RGBMatrixOptions()
+    options.rows = 32
+    options.chain_length = 4
+    options.pwm_bits = 3
+    options.pwm_lsb_nanoseconds = 300
+    options.gpio_slowdown = 2
+    return RGBMatrix(options = options)
+
+
+def draw_display(canvas, font_file, game):
+    height_delta = 8
+    width_delta = 6
+
+    total_width = 128
+
+    font = graphics.Font()
+    font.LoadFont(font_file)
+    red_color = graphics.Color(255,0,0)
+
+    canvas.Clear()
+    graphics.DrawText(canvas, font, 0, 7, red_color, game['home_team'] + " @ " + game['away_team'])
